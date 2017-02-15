a8448c7c8cc52a40c5d5a56089199355dce71b8f
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-02-15 15:08:59 -0600

    Make maze size adjustable from controls.

--- src/main/java/maze/InteractiveMazeGame.java
+++ src/main/java/maze/InteractiveMazeGame.java
@@ -1,5 +1,8 @@
 package maze;
 
+import java.awt.Point;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import maze.room.Room;
 import maze.output.OutputConsumer;
 import org.openide.util.Lookup;
@@ -12,6 +15,9 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service = OutputConsumer.class)
 public class InteractiveMazeGame extends javax.swing.JFrame
         implements OutputConsumer {
+
+    private static final Logger LOG
+            = Logger.getLogger(InteractiveMazeGame.class.getSimpleName());
 
     /**
      * Creates new form InteractiveMazeGame
@@ -241,87 +247,34 @@ public class InteractiveMazeGame extends javax.swing.JFrame
             }
         }
         Maze maze = factory.makeMaze();
-        Room room1 = factory.makeRoom(1);
-        Room room2 = factory.makeRoom(2);
-        Room room3 = factory.makeRoom(3);
-        Room room4 = factory.makeRoom(4);
-        Room room5 = factory.makeRoom(5);
-        Room room6 = factory.makeRoom(6);
-        Room room7 = factory.makeRoom(7);
-        Room room8 = factory.makeRoom(8);
-        Room room9 = factory.makeRoom(9);
-        Door door1 = factory.makeDoor(room1, room2);
-        Door door2 = factory.makeDoor(room2, room3);
-        Door door3 = factory.makeDoor(room4, room5);
-        Door door4 = factory.makeDoor(room5, room6);
-        Door door5 = factory.makeDoor(room5, room8);
-        Door door6 = factory.makeDoor(room6, room9);
-        Door door7 = factory.makeDoor(room7, room8);
-        Door door8 = factory.makeDoor(room1, room4);
-
-        door1.setOpen(true);
-        door2.setOpen(false);
-        door3.setOpen(true);
-        door4.setOpen(true);
-        door5.setOpen(false);
-        door6.setOpen(true);
-        door7.setOpen(true);
-        door8.setOpen(true);
-
-        room1.setSide(Direction.NORTH, door8);
-        room1.setSide(Direction.EAST, factory.makeWall());
-        room1.setSide(Direction.SOUTH, factory.makeWall());
-        room1.setSide(Direction.WEST, door1);
-
-        room2.setSide(Direction.NORTH, factory.makeWall());
-        room2.setSide(Direction.EAST, door1);
-        room2.setSide(Direction.SOUTH, factory.makeWall());
-        room2.setSide(Direction.WEST, door2);
-
-        room3.setSide(Direction.NORTH, factory.makeWall());
-        room3.setSide(Direction.EAST, door2);
-        room3.setSide(Direction.SOUTH, factory.makeWall());
-        room3.setSide(Direction.WEST, factory.makeWall());
-
-        room4.setSide(Direction.NORTH, factory.makeWall());
-        room4.setSide(Direction.EAST, factory.makeWall());
-        room4.setSide(Direction.SOUTH, door8);
-        room4.setSide(Direction.WEST, door3);
-
-        room5.setSide(Direction.NORTH, door5);
-        room5.setSide(Direction.EAST, door3);
-        room5.setSide(Direction.SOUTH, factory.makeWall());
-        room5.setSide(Direction.WEST, door4);
-
-        room6.setSide(Direction.NORTH, door6);
-        room6.setSide(Direction.EAST, door4);
-        room6.setSide(Direction.SOUTH, factory.makeWall());
-        room6.setSide(Direction.WEST, factory.makeWall());
-
-        room7.setSide(Direction.NORTH, factory.makeWall());
-        room7.setSide(Direction.EAST, factory.makeWall());
-        room7.setSide(Direction.SOUTH, factory.makeWall());
-        room7.setSide(Direction.WEST, door7);
-
-        room8.setSide(Direction.NORTH, factory.makeWall());
-        room8.setSide(Direction.EAST, door7);
-        room8.setSide(Direction.SOUTH, door5);
-        room8.setSide(Direction.WEST, factory.makeWall());
-
-        room9.setSide(Direction.NORTH, factory.makeWall());
-        room9.setSide(Direction.EAST, factory.makeWall());
-        room9.setSide(Direction.SOUTH, door6);
-        room9.setSide(Direction.WEST, factory.makeWall());
-
-        maze.addRoom(room1);
-        maze.addRoom(room2);
-        maze.addRoom(room3);
-        maze.addRoom(room4);
-        maze.addRoom(room5);
-        maze.addRoom(room6);
-        maze.addRoom(room7);
-        maze.addRoom(room8);
-        maze.addRoom(room9);
+        Room[][] rooms = new Room[row][col];
+        int count = 0;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                rooms[r][c] = factory.makeRoom(++count);
+                //Add walls on borders of the room
+                rooms[r][c].setSide(Direction.NORTH,
+                        factory.makeWall());
+                rooms[r][c].setSide(Direction.WEST,
+                        factory.makeWall());
+                rooms[r][c].setSide(Direction.SOUTH,
+                        factory.makeWall());
+                rooms[r][c].setSide(Direction.EAST,
+                        factory.makeWall());
+                rooms[r][c].setLocation(new Point(c, r));
+                //Link to nearby rooms
+                if (c > 0) {
+                    //Has a room to the left
+                    rooms[r][c - 1].setSide(Direction.WEST.opposite(), null);
+                }
+                if (r > 0) {
+                    //Has a room to on top
+                    rooms[r - 1][c].setSide(Direction.NORTH.opposite(), null);
+                }
+                maze.addRoom(rooms[r][c]);
+            }
+        }
+        LOG.log(Level.INFO, "Added {0} rooms", count);
         maze.setCurrentRoom(1);
         jScrollPane1.setViewportView(new Maze.MazePanel(maze));
     }//GEN-LAST:event_generateActionPerformed
